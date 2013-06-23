be21f622b3813afd8ba79732c31c80ff7488f82a
Alex Goretoy <alex@goretoy.com>
2013-06-23 01:28:00 -0700

    updated providers in CreditcardNumberGeneratorServiceProvider.php

--- src/Gxela/CreditcardNumberGenerator/CreditcardNumberGeneratorServiceProvider.php
+++ src/Gxela/CreditcardNumberGenerator/CreditcardNumberGeneratorServiceProvider.php
@@ -21,24 +21,27 @@ class CreditcardNumberGeneratorServiceProvider extends ServiceProvider {
         $this->package('gxela/creditcard-number-generator');
     }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['credit_card_generator'] = $this->app->share(function() { return new CreditCardGenerator(); });
+        $this->app['credit_card_generator.visa'] = $this->app->share(function() { return CreditCardGenerator::get_visa16(); });
+        $this->app['credit_card_generator.mastercard'] = $this->app->share(function() { return CreditCardGenerator::get_mastercard(); });
+        $this->app['credit_card_generator.amex'] = $this->app->share(function() { return CreditCardGenerator::get_amex(); });
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('credit_card_generator','credit_card_generator.visa', 'credit_card_generator.mastercard', 'credit_card_generator.amex');
+    }
 
 }
