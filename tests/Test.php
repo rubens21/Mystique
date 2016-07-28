62e6f89b67f8c880501e772338bce82271a89f95
Rubens <rubens21@gmail.com>
2016-07-28 18:31:57 -0300

    Basic credit card test

--- tests/Test.php
+++ tests/Test.php
@@ -11,6 +11,10 @@ class Test extends PHPUnit_Framework_TestCase
 
     public function testCcdGenerated()
     {
-        
+        $ccd = new \Mystique\CreditCard();
+        $this->assertRegExp('/\d+/', $ccd->getNumber());
+        $this->assertLessThan(13, $ccd->getValidMonth());
+        $this->assertGreaterThan(0, $ccd->getValidMonth());
+        $this->assertGreaterThanOrEqual(date('Y'), $ccd->getValidYear());
     }
 }
